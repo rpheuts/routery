e291200e56373ce0d27e5d4cc1d427324a9e2d08
Robert Heuts <robert.heuts@popagency.com>
2015-10-20 15:11:00 -0700

    Added LDAP Auth support

--- authentication/ldapauthprovider.go
+++ authentication/ldapauthprovider.go
@@ -1,5 +1,11 @@
 package authentication
 
+import (
+	"gopkg.in/ldap.v2"
+	"fmt"
+	"log"
+)
+
 type LDAPAuthProvider struct {
 	config *AuthConfig
 }
@@ -11,5 +17,54 @@ func (ap *LDAPAuthProvider) Initialize(config *AuthConfig) error {
 }
 
 func (ap *LDAPAuthProvider) Authenticate(username string, password string) bool {
+	return ap.LDAPAuthenticate(username, password)
+}
+
+func (ap *LDAPAuthProvider) LDAPAuthenticate(username string, password string) bool {
+	bindusername := ap.config.Username
+	bindpassword := ap.config.Password
+
+	l, err := ldap.Dial("tcp", fmt.Sprintf("%s:%d", ap.config.Hostname, ap.config.Port))
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	defer l.Close()
+
+	// First bind with a read only user
+	err = l.Bind(bindusername, bindpassword)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+
+	// Search for the given username
+	searchRequest := ldap.NewSearchRequest(
+		ap.config.Arguments,
+		ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
+		fmt.Sprintf("(&(objectClass=person)(mailNickname=%s))", username),
+		[]string{"dn"},
+		nil,
+	)
+
+	sr, err := l.Search(searchRequest)
+	if err != nil {
+		log.Println(err)
+		return false;
+	}
+
+	if len(sr.Entries) != 1 {
+		return false
+	}
+
+	userdn := sr.Entries[0].DN
+
+	// Bind as the user to verify their password
+	err = l.Bind(userdn, password)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+
 	return true
 }
