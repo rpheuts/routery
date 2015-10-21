b16e982b11b4f03bb135fcf5304216ba4079c480
Robert Heuts <robert.heuts@popagency.com>
2015-10-20 17:33:40 -0700

    Re-added example config file Removed unsupported cipher suites

--- frontends/forwardfrontend.go
+++ frontends/forwardfrontend.go
@@ -111,12 +111,10 @@ func (ff *ForwardFrontend) getCACert() *tls.Config {
 	mTLSConfig := &tls.Config{
 		CipherSuites: []uint16{
 			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
 			tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
 			tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
 			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
 		},
 	}
 
